df7571d4ec8e4a2caf41b8713471d43ed7c179ce
floh-bhaptics <floh@fahrenbergers.de>
2022-06-19 08:45:46 +0200

    Projektdateien hinzufügen.

---
+++ ShadowgateVR_bhaptics.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using HarmonyLib;
+using MyBhapticsTactsuit;
+
+namespace ShadowgateVR_bhaptics
+{
+    public class ShadowgateVR_bhaptics : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            tactsuitVr = new TactsuitVR();
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+        }
+
+        [HarmonyPatch(typeof(Player), "Die", new Type[] { typeof(DeathType), typeof(DamageType) })]
+        public class bhaptics_PlayerDies
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.StopThreads();
+            }
+        }
+
+    }
+}
