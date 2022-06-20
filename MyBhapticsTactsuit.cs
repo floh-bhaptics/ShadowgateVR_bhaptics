24a11639eb16e1ea5a03e055cf27aff616f9aeb2
floh-bhaptics <floh@fahrenbergers.de>
2022-06-20 13:00:33 +0200

    First nicely working version

--- MyBhapticsTactsuit.cs
+++ MyBhapticsTactsuit.cs
@@ -124,30 +124,14 @@ namespace MyBhapticsTactsuit
             bHaptics.SubmitRegistered(key, key, scaleOption, rotationOption);
         }
 
-        public void Recoil(string weaponName, bool isRightHand, float intensity = 1.0f)
+        public void GetItem(bool isRightHand)
         {
-            // weaponName is a parameter that will go into the vest feedback pattern name
-            // isRightHand is just which side the feedback is on
-            // intensity should usually be between 0 and 1
-
-            float duration = 1.0f;
-            var scaleOption = new bHaptics.ScaleOption(intensity, duration);
-            // the function needs some rotation if you want to give the scale option as well
-            var rotationFront = new bHaptics.RotationOption(0f, 0f);
-            // make postfix according to parameter
             string postfix = "_L";
             if (isRightHand) { postfix = "_R"; }
 
-            // stitch together pattern names for Arm and Hand recoil
-            string keyHands = "RecoilHands" + postfix;
-            string keyArm = "Recoil" + postfix;
-            // vest pattern name contains the weapon name. This way, you can quickly switch
-            // between swords, pistols, shotguns, ... by just changing the shoulder feedback
-            // and scaling via the intensity for arms and hands
-            string keyVest = "Recoil" + weaponName + "Vest" + postfix;
-            bHaptics.SubmitRegistered(keyHands, keyHands, scaleOption, rotationFront);
-            bHaptics.SubmitRegistered(keyArm, keyArm, scaleOption, rotationFront);
-            bHaptics.SubmitRegistered(keyVest, keyVest, scaleOption, rotationFront);
+            string keyWand = "GetWand" + postfix;
+
+            PlaybackHaptics(keyWand);
         }
 
         public void CastSpell(string spellName, bool isRightHand, float intensity = 1.0f)
--- ShadowgateVR_bhaptics.cs
+++ ShadowgateVR_bhaptics.cs
@@ -12,6 +12,7 @@ namespace ShadowgateVR_bhaptics
     public class ShadowgateVR_bhaptics : MelonMod
     {
         public static TactsuitVR tactsuitVr;
+        public static bool wandIsRight = true;
 
         public override void OnApplicationStart()
         {
@@ -27,6 +28,152 @@ namespace ShadowgateVR_bhaptics
             public static void Postfix()
             {
                 tactsuitVr.StopThreads();
+            }
+        }
+
+        [HarmonyPatch(typeof(Player), "ApplyHealing", new Type[] { typeof(int) })]
+        public class bhaptics_PlayerHeal
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Player __instance)
+            {
+                tactsuitVr.PlaybackHaptics("Healing");
+                if (__instance.HP >= 0.3f * (int)__instance.maxHP) tactsuitVr.StopHeartBeat();
+            }
+        }
+
+        [HarmonyPatch(typeof(Player), "OnDamaged", new Type[] { typeof(int), typeof(DamageType) })]
+        public class bhaptics_PlayerDamage
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Player __instance, DamageType type)
+            {
+                string pattern = "Impact";
+                switch (type)
+                {
+                    case DamageType.None:
+                        pattern = "Impact";
+                        break;
+                    case DamageType.Fire:
+                        pattern = "Burning";
+                        break;
+                    case DamageType.Cold:
+                        pattern = "Shiver";
+                        break;
+                    case DamageType.Lightning:
+                        pattern = "Lightning";
+                        break;
+                    case DamageType.Poison:
+                        pattern = "Poison";
+                        break;
+                    default:
+                        pattern = "Impact";
+                        break;
+                }
+                tactsuitVr.PlaybackHaptics(pattern);
+                if (__instance.HP < 0.3f * (int)__instance.maxHP) tactsuitVr.StartHeartBeat();
+            }
+        }
+
+        [HarmonyPatch(typeof(Inventory), "SetDominantHand", new Type[] { typeof(HandMode) })]
+        public class bhaptics_SetDominantHand
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Inventory __instance, HandMode hand)
+            {
+                wandIsRight = (hand == HandMode.Right);
+            }
+        }
+
+        [HarmonyPatch(typeof(InventorySlotBelt), "GrabWith", new Type[] { typeof(Hand) })]
+        public class bhaptics_GetInventoryItem
+        {
+            [HarmonyPostfix]
+            public static void Postfix(InventorySlotBelt __instance, BeltItem ____itemInSlot)
+            {
+                tactsuitVr.LOG("Grabbed: " + ____itemInSlot.name);
+                if (____itemInSlot.name == "Wand") tactsuitVr.GetItem(wandIsRight);
+                if (____itemInSlot.name == "Shield") tactsuitVr.GetItem(!wandIsRight);
+            }
+        }
+
+        [HarmonyPatch(typeof(InventorySlotBelt), "PlaceItem", new Type[] { typeof(BeltItem), typeof(bool) })]
+        public class bhaptics_PlaceInventoryItem
+        {
+            [HarmonyPostfix]
+            public static void Postfix(InventorySlotBelt __instance, BeltItem item)
+            {
+                if (item.name.Contains("Wand")) tactsuitVr.GetItem(wandIsRight);
+                if (item.name.Contains("Shield")) tactsuitVr.GetItem(!wandIsRight);
+            }
+        }
+
+        [HarmonyPatch(typeof(InventorySlotBelt), "RemoveItem", new Type[] { typeof(bool) })]
+        public class bhaptics_RemoveInventoryItem
+        {
+            [HarmonyPostfix]
+            public static void Postfix(InventorySlotBelt __instance, BeltItem ____itemInSlot)
+            {
+                tactsuitVr.LOG("Removed: " + ____itemInSlot.name);
+                if (____itemInSlot.name.Contains("Wand")) tactsuitVr.GetItem(wandIsRight);
+                if (____itemInSlot.name.Contains("Shield")) tactsuitVr.GetItem(!wandIsRight);
+            }
+        }
+
+        [HarmonyPatch(typeof(Wand), "TriggerPress", new Type[] { typeof(bool) })]
+        public class bhaptics_WandCast
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Wand __instance, bool pressed, Hand ____hand)
+            {
+                if ((!pressed)) return;
+                bool isRightHand = ____hand.IsRightHand();
+                tactsuitVr.CastSpell("Fire", isRightHand);
+            }
+        }
+
+        [HarmonyPatch(typeof(Shield), "TriggerPress", new Type[] { typeof(bool) })]
+        public class bhaptics_ShieldCast
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Shield __instance, bool pressed, Hand ____hand, bool ____isCharged, bool ____isEnabled)
+            {
+                if ((!pressed) || (!____isCharged) || (!____isEnabled)) return;
+                bool isRightHand = ____hand.IsRightHand();
+                tactsuitVr.CastSpell("Barrier", isRightHand);
+            }
+        }
+
+        [HarmonyPatch(typeof(LocomotionTeleport), "DoTeleport", new Type[] {  })]
+        public class bhaptics_Teleport
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.LOG("Instant");
+                tactsuitVr.PlaybackHaptics("TeleportThrough");
+            }
+        }
+
+        [HarmonyPatch(typeof(TeleportTransitionWarp), "DoWarp", new Type[] { })]
+        public class bhaptics_TeleportWarp
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.LOG("Warp");
+                tactsuitVr.PlaybackHaptics("TeleportThrough");
+            }
+        }
+
+        [HarmonyPatch(typeof(TeleportTransitionBlink), "BlinkCoroutine", new Type[] { })]
+        public class bhaptics_TeleportBlink
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.LOG("Blink");
+                tactsuitVr.PlaybackHaptics("TeleportThrough");
             }
         }
 
